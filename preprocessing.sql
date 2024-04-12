aa8c2e31166e9baeda621f98efbce23292e70b01
amirtha19 <amirthavarshini1901@gmail.com>
2024-04-13 00:26:21 +0530

    modify

--- preprocessing.sql
+++ preprocessing.sql
@@ -1,81 +1,74 @@
+USE Instacart;
 
-use Instacart;
+-- Data Integrity
 
---Data Integrity
-
--- Count the number of missing values in the 'days_since_prior_order' 
+-- Count the number of missing values in the 'days_since_prior_order' column
 SELECT COUNT(*) AS MissingValues FROM ORDERS WHERE days_since_prior_order IS NULL;
 
--- Remove duplicate entries in the 'products' 
-DELETE FROM products WHERE product_id NOT IN ( SELECT MIN(product_id) FROM products GROUP BY product_name );
+-- Remove duplicate entries in the 'products' table
+DELETE FROM products WHERE product_id NOT IN (SELECT MIN(product_id) FROM products GROUP BY product_name);
 
--- Remove duplicate entries in the 'PRODUCTS' 
-DELETE FROM PRODUCTS WHERE product_id NOT IN ( SELECT MIN(product_id) FROM PRODUCTs GROUP BY product_name);
+-- Remove duplicate entries in the 'PRODUCTS' table
+DELETE FROM PRODUCTS WHERE product_id NOT IN (SELECT MIN(product_id) FROM PRODUCTS GROUP BY product_name);
 
--- Set NULL values in the 'days_since_prior_order' 
+-- Set NULL values in the 'days_since_prior_order' column
 UPDATE ORDERS SET days_since_prior_order = NULL WHERE days_since_prior_order = '';
 
--- Set NULL values in the 'order_hour_of_day' 
+-- Set NULL values in the 'order_hour_of_day' column
 UPDATE ORDERS SET order_hour_of_day = NULL WHERE order_hour_of_day = '';
 
+-- Aisles
 
---- Asiles
+SELECT COUNT(*) AS aisle_count FROM aisles;
 
-select count(*) as aisle_count from aisles;
+-- Department
 
---- Department
+SELECT COUNT(*) AS department_count FROM departments;
 
-select count(*) as department_count from departments
+-- Products
 
----- Products
+SELECT COUNT(*) AS product_count FROM products;
 
-select count(*) as product_count from products
+-- Orders
 
---- Orders
+SELECT COUNT(*) AS orders_count FROM orders;
 
-select count(*) as orders_count from orders;
-
---- JOIN PRODUCTS AND ORDERS
+-- JOIN PRODUCTS AND ORDERS
 SELECT *
 FROM aisles
 WHERE PATINDEX('%[a-zA-Z]%', aisle_id) > 0;
 
 -- Delete the rows where keys are mixed with strings
 DELETE FROM products
-WHERE PATINDEX('%[a-zA-Z]%', department_id) > 0 or PATINDEX('%[a-zA-Z]%', aisle_id) > 0;
+WHERE PATINDEX('%[a-zA-Z]%', department_id) > 0 OR PATINDEX('%[a-zA-Z]%', aisle_id) > 0;
 
-Alter table products
-Alter column department_id int;
+-- Alter table products
+ALTER TABLE products ALTER COLUMN department_id INT;
 
-alter VIEW curr_orders AS 
-(select orders.order_id,user_id,products.product_id,order_number,order_dow,order_hour_of_day,days_since_prior_order,add_to_cart_order,product_name,aisle,department,reordered
-from orders
-inner join order_products__train
-on orders.order_id = order_products__train.order_id
-inner join products
-on products.product_id=order_products__train.product_id
-inner join departments
-on departments.department_id = products.department_id
-inner join aisles
-on aisles.aisle_id = products.aisle_id);
+-- Alter view curr_orders
+ALTER VIEW curr_orders AS
+(SELECT orders.order_id, user_id, products.product_id, order_number, order_dow, order_hour_of_day, days_since_prior_order, add_to_cart_order, product_name, aisle, department, reordered
+FROM orders
+INNER JOIN order_products__train ON orders.order_id = order_products__train.order_id
+INNER JOIN products ON products.product_id = order_products__train.product_id
+INNER JOIN departments ON departments.department_id = products.department_id
+INNER JOIN aisles ON aisles.aisle_id = products.aisle_id);
 
-alter view prior_orders as (select orders.order_id,user_id,products.product_id,order_number,order_dow,order_hour_of_day,days_since_prior_order,add_to_cart_order,product_name,aisle,department,reordered
-from orders
-inner join order_products__prior
-on orders.order_id = order_products__prior.order_id
-inner join products
-on products.product_id=order_products__prior.product_id
-inner join departments
-on departments.department_id = products.department_id
-inner join aisles
-on aisles.aisle_id = products.aisle_id);
+-- Alter view prior_orders
+ALTER VIEW prior_orders AS
+(SELECT orders.order_id, user_id, products.product_id, order_number, order_dow, order_hour_of_day, days_since_prior_order, add_to_cart_order, product_name, aisle, department, reordered
+FROM orders
+INNER JOIN order_products__prior ON orders.order_id = order_products__prior.order_id
+INNER JOIN products ON products.product_id = order_products__prior.product_id
+INNER JOIN departments ON departments.department_id = products.department_id
+INNER JOIN aisles ON aisles.aisle_id = products.aisle_id);
 
---- View null values
+-- View null values
 SELECT COUNT(*)
 FROM orders
 WHERE days_since_prior_order IS NULL;
 
-select top 5 *
-from prior_orders
-where product_id is null
-
+-- Select top 5 rows from prior_orders where product_id is null
+SELECT TOP 5 *
+FROM prior_orders
+WHERE product_id IS NULL;
